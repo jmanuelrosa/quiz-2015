5f9c8f2b9dbf3597ed1badfbba1a49f6e44644ce
Jose Manuel Rosa <josemanuel.rosamoncayo@gmail.com>
2015-07-14 17:33:01 +0200

    Seccion de autor añadida como complemento

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/author', function(req, res) {
+    res.render('author', {
+        author: {
+            name: 'José Manuel Rosa Moncayo',
+            photo: 'https://secure.gravatar.com/avatar/bef83d63fae4a2f8b7f038654df9b771?size=496'
+        }
+    })
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
